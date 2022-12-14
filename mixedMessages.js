25df9137e3f8c38ed922a6c761d52ce6724327f7
Julia Linnea <116358572+juli-nea@users.noreply.github.com>
2022-12-14 20:22:59 +0100

    First commit

---
+++ mixedMessages.js
@@ -0,0 +1,10 @@
+
+// Expected output is "Your ideal animal companion is [physical adjective] [emotional adjective] [animal]!"
+
+const physArr = [];
+const emotArr = [];
+const animal = [];
+
+
+
+
