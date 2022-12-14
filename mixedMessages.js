5ac1aa43ef4a6f5609079bad8535e4f51afada9c
Julia Linnea <116358572+juli-nea@users.noreply.github.com>
2022-12-14 20:29:40 +0100

    add words to arrays

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,9 +1,9 @@
 
 // Expected output is "Your ideal animal companion is [physical adjective] [emotional adjective] [animal]!"
 
-const physArr = [];
-const emotArr = [];
-const animal = [];
+const physArr = ['a blue', 'a fat', 'a small', 'a huge', 'a chunky', 'a green', 'a fluffy'];
+const emotArr = ['lazy', 'happy', 'funny', 'funky', 'confused', 'aggressive', 'curious'];
+const animal = ['chihuahua', 'kitty', 'bear', 'monkey', 'dog', 'elephant', 'snake'];
 
 
 
