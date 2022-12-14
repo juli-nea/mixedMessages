83836ba240891ed784ec4f7b2ce180cc04545b53
Julia Linnea <116358572+juli-nea@users.noreply.github.com>
2022-12-14 20:44:15 +0100

    Add randomly selected words

--- mixedMessages.js
+++ mixedMessages.js
@@ -3,8 +3,22 @@
 
 const physArr = ['a blue', 'a fat', 'a small', 'a huge', 'a chunky', 'a green', 'a fluffy'];
 const emotArr = ['lazy', 'happy', 'funny', 'funky', 'confused', 'aggressive', 'curious'];
-const animal = ['chihuahua', 'kitty', 'bear', 'monkey', 'dog', 'elephant', 'snake'];
+const animalArr = ['chihuahua', 'kitty', 'bear', 'monkey', 'dog', 'elephant', 'snake'];
+
+// Randomly select target words
 
 
+const selectPhys = () => {
+    let physical = physArr[Math.floor(Math.random()*physArr.length)]
+    return physical
+}
 
+const selectEmot = () => {
+    let emotional = emotArr[Math.floor(Math.random()*emotArr.length)]
+    return emotional
+}
 
+const selectAnimal = () => {
+    let animal = animalArr[Math.floor(Math.random()*animalArr.length)]
+    return animal
+}
